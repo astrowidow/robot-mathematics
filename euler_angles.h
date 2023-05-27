47d0cfcb8f9c853e2e8e9e7c5984476094b33186
Kyosuke Tawara <astrowidow@gmail.com>
2023-05-28 08:25:41 +0900

    オイラー角のニューを追加

--- euler_angles.h
+++ euler_angles.h
@@ -15,4 +15,25 @@ typedef struct {
     RotationOrder type;
 } EulerAngles;
 
+/**
+ * @brief Function to create euler angles
+ * 
+ * @param x Value of the x component
+ * @param y Value of the y component
+ * @param z Value of the z component
+ * @param type rotational sequence
+ * @return euler angles
+ */
+EulerAngles EulerAngles_create(const double x, const double y, const double z, const RotationOrder type)
+{
+    EulerAngles euler;
+
+    euler.x = x;
+    euler.y = y;
+    euler.z = z;
+    euler.type = type;
+
+    return euler;
+}
+
 #endif
